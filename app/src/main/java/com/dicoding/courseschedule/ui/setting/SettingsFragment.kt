b54dce4fd134153a52d535688a6aa133c6814b66
ricky-kiva <ricky.kiva@gmail.com>
2023-07-02 19:45:42 +0700

    add theme update functionality

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,6 +2,8 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.courseschedule.R
 
@@ -9,8 +11,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        //TODO 10 : Update theme based on value in ListPreference
-        //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        // XTODO 10 : Update theme based on value in ListPreference
+
+        val listPreference = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+        listPreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                val nightMode = when (newValue) {
+                    getString(R.string.pref_dark_auto) -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    getString(R.string.pref_dark_on) -> AppCompatDelegate.MODE_NIGHT_YES
+                    getString(R.string.pref_dark_off) -> AppCompatDelegate.MODE_NIGHT_NO
+                    else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                }
+                updateTheme(nightMode)
+                true
+            }
+
+        // TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
