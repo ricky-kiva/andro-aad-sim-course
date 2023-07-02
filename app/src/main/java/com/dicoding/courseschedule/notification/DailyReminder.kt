02aee5340ab5004929084e62dfdf2ce7f6dbb647
ricky-kiva <ricky.kiva@gmail.com>
2023-07-02 21:09:02 +0700

    add showNotification function

--- app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
+++ app/src/main/java/com/dicoding/courseschedule/notification/DailyReminder.kt
@@ -1,14 +1,21 @@
 package com.dicoding.courseschedule.notification
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
 import com.dicoding.courseschedule.data.DataRepository
+import com.dicoding.courseschedule.ui.home.HomeActivity
+import com.dicoding.courseschedule.util.NOTIFICATION_CHANNEL_ID
+import com.dicoding.courseschedule.util.NOTIFICATION_CHANNEL_NAME
+import com.dicoding.courseschedule.util.NOTIFICATION_ID
 import com.dicoding.courseschedule.util.executeThread
 import java.util.*
 
@@ -17,7 +24,8 @@ class DailyReminder : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         executeThread {
             val repository = DataRepository.getInstance(context)
-            val courses = repository?.getTodaySchedule()
+            val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+            val courses = repository?.getTodaySchedule(dayOfWeek)
 
             courses?.let {
                 if (it.isNotEmpty()) showNotification(context, it)
@@ -55,12 +63,34 @@ class DailyReminder : BroadcastReceiver() {
     }
 
     private fun showNotification(context: Context, content: List<Course>) {
-        //TODO 13 : Show today schedules in inbox style notification & open HomeActivity when notification tapped
+        // XTODO 13 : Show today schedules in inbox style notification & open HomeActivity when notification tapped
         val notificationStyle = NotificationCompat.InboxStyle()
         val timeString = context.resources.getString(R.string.notification_message_format)
         content.forEach {
             val courseData = String.format(timeString, it.startTime, it.endTime, it.courseName)
             notificationStyle.addLine(courseData)
         }
+
+        val intent = Intent(context, HomeActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_NAME)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setStyle(notificationStyle)
+            .setContentTitle(context.getString(R.string.today_schedule))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            notification.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
     }
 }
